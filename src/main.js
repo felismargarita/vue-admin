1cb9e07f4d3952276e25fb1b8b99d926b6850b08
felis <felismargarita@hotmail.com>
2021-07-02 22:11:53 +0800

    feature:引入组件库Antd Vue

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.config.productionTip = false
+app.use(Antd)
+app.use(store).use(router).mount('#app')
